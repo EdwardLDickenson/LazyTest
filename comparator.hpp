27d9b973d01274a61bd969a798afb295fa5f43f5
Edward Lloyd Dickenson <EdwardLDickenson@users.noreply.github.com>
2019-09-27 23:59:28 -0500

    GCC/Clang Assert, naming
    *Fixed an error with the Assert class which caused GCC to fail compilation.
    *Changed naming conventions for file names to be all lower case

---
+++ comparator.hpp
@@ -0,0 +1,64 @@
+#ifndef COMPARATOR_HPP
+#define COMPARATOR_HPP
+
+enum Comparator {
+	//	Unary operators
+	isNull,
+	notNull,
+
+	//	Binary operators
+	equal,
+	unequal,
+	lessThan,
+	greaterThan,
+	lessThanEqual,
+	greaterThanEqual
+	//isType
+};
+
+//  Should create a unicode/multicharacter version of this function.
+//  For example the <= sign could be replaced with a single character. 
+//  But that would hurt compatibility while helping readability.
+string getComparatorString(Comparator compareType)
+{
+	switch(compareType)
+	{
+		case Comparator::equal:
+			return "==";
+		break;
+		case::Comparator::unequal:
+			return "!=";
+		break;
+		case::Comparator::lessThan:
+			return "<";
+		break;
+		case::Comparator::greaterThan:
+			return ">";
+		break;
+		case::Comparator::lessThanEqual:
+			return "<=";
+		break;
+		case::Comparator::greaterThanEqual:
+			return ">=";
+		break;
+        //  Ø is not 0. It is empty set
+		case::Comparator::isNull:
+			return "u8\2205";
+		break;
+        //  Ø is not 0. It is empty set
+		case::Comparator::notNull:
+			return "!u8\2205";
+		break;
+	}
+}
+
+#endif	//	COMPARATOR_HPP
+
+
+
+//	TODO:
+//
+//
+//
+
+
